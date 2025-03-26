5acfda24affb634364f724f225dd96aa54848fd9
w4keupvan <203548283+w4keupvan@users.noreply.github.com>
2025-03-26 14:11:13 +0800

    feat: add sqlc dependencies for generating queries

---
+++ backend/sql/queries/users.sql
@@ -0,0 +1,22 @@
+-- name: GetUserByEmail :one
+SELECT * FROM users
+WHERE email = $1 LIMIT 1;
+
+-- name: CreateUser :one
+INSERT INTO users (
+    name, email, password
+) VALUES (
+    $1, $2, $3
+)
+RETURNING *;
+
+-- name: UpdateUser :exec
+UPDATE users
+    set name = $1,
+    email = $2,
+    password = $3
+WHERE id = $1;
+
+-- name: DeleteUser :exec
+DELETE FROM users
+WHERE id = $1;
---
+++ backend/sql/schemas/00002_create_users.sql
@@ -0,0 +1,17 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS users(
+    id BIGSERIAL PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL,
+    email CITEXT UNIQUE NOT NULL,
+    password bytea NOT NULL,
+    created_at TIMESTAMP DEFAULT current_timestamp,
+    updated_at TIMESTAMP DEFAULT current_timestamp,
+    version BIGINT
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS users;
+-- +goose StatementEnd
