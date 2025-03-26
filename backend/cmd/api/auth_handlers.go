5d18a3f146cdfbe07c8a64240e664d617ae74efd
w4keupvan <203548283+w4keupvan@users.noreply.github.com>
2025-03-26 22:42:10 +0800

    feat: add login handler

--- backend/cmd/api/auth_handlers.go
+++ backend/cmd/api/auth_handlers.go
@@ -6,10 +6,67 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/golang-jwt/jwt"
 	"github.com/jackc/pgx/v5"
 	"github.com/w4keupvan/gym-ops/backend/internal/database"
 	"github.com/w4keupvan/gym-ops/backend/internal/validator"
 )
+
+func (app *application) loginHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Email     string              `json:"email"`
+		Password  string              `json:"password"`
+		Validator validator.Validator `json:"-"`
+	}
+
+	err := app.decodeJSON(w, r, &input, true)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	input.Validator.CheckField(input.Email != "", "email", "email is required")
+	input.Validator.CheckField(input.Password != "", "password", "password is required")
+
+	input.Validator.CheckField(validator.IsEmail(input.Email), "email", "invalid email format")
+	input.Validator.CheckField(len(input.Password) > 8, "password", "password must be greater than 8 characters")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	user, err := app.db.GetUserByEmail(ctx, input.Email)
+	existingEmail := errors.Is(err, pgx.ErrNoRows)
+
+	if err != nil && !existingEmail {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	input.Validator.CheckField(!existingEmail, "email", "no account found with this email")
+
+	if input.Validator.HasErrors() {
+		app.failedValidationResponse(w, r, input.Validator)
+		return
+	}
+
+	claims := userClaims{
+		ID: user.ID,
+		StandardClaims: jwt.StandardClaims{
+			Issuer:    "gym-ops",
+			IssuedAt:  time.Now().Unix(),
+			ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
+		},
+	}
+
+	token, err := app.generateUserToken(app.config, claims)
+	if err != nil {
+		app.logger.Error(err.Error())
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	app.writeJSON(w, http.StatusOK, envelope{"token": token}, nil)
+}
 
 func (app *application) registerHandler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
@@ -25,6 +82,14 @@ func (app *application) registerHandler(w http.ResponseWriter, r *http.Request) 
 		return
 	}
 
+	input.Validator.CheckField(input.Name != "", "name", "name field is required")
+	input.Validator.CheckField(input.Email != "", "email", "email field is required")
+	input.Validator.CheckField(input.Password != "", "password", "password field is required")
+
+	input.Validator.CheckField(len(input.Name) > 5, "name", "name must be greater than 5 characters")
+	input.Validator.CheckField(validator.IsEmail(input.Email), "email", "invalid email format")
+	input.Validator.CheckField(len(input.Password) > 8, "password", "password must be greater than 8 characters")
+
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
@@ -36,14 +101,6 @@ func (app *application) registerHandler(w http.ResponseWriter, r *http.Request) 
 	}
 
 	input.Validator.CheckField(nonExistingEmail, "email", "email is already taken")
-
-	input.Validator.CheckField(input.Name != "", "name", "name field is required")
-	input.Validator.CheckField(input.Email != "", "email", "email field is required")
-	input.Validator.CheckField(input.Password != "", "password", "password field is required")
-
-	input.Validator.CheckField(len(input.Name) > 5, "name", "name must be greater than 5 characters")
-	input.Validator.CheckField(validator.IsEmail(input.Email), "email", "invalid email format")
-	input.Validator.CheckField(len(input.Password) > 8, "password", "password must be greater than 8 characters")
 
 	if input.Validator.HasErrors() {
 		app.failedValidationResponse(w, r, input.Validator)
