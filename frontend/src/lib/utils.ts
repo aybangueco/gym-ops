a2b91c0964aa0547a28ee6c6bccdddfaa7026d1d
soapermanwun <203548283+soapermanwun@users.noreply.github.com>
2025-04-19 22:33:23 +0800

    feat: add pagination component

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,19 @@
 import { type ClassValue, clsx } from 'clsx';
+import { readable } from 'svelte/store';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
+}
+
+// eslint-disable-next-line
+// @ts-ignore
+export function runes_to_store(cb) {
+	readable(cb, (set) =>
+		$effect.root(() => {
+			$effect.pre(() => {
+				set(cb());
+			});
+		})
+	);
 }
