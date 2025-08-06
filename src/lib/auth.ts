85d41e783a0b4e6dcb36be2f99fcb1f0250880c3
aybangueco <203548283+aybangueco@users.noreply.github.com>
2025-08-06 21:18:41 +0800

    feat: add temporary configuration for auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -8,6 +8,8 @@ export const auth = betterAuth({
     provider: 'postgresql'
   }),
   emailAndPassword: {
-    enabled: true
+    enabled: true,
+    autoSignIn: false,
+    requireEmailVerification: false
   }
 })
