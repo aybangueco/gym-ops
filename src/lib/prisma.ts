62e1398c5c94288bd88b08e8c37c96707c7efc4c
aybangueco <203548283+aybangueco@users.noreply.github.com>
2025-08-19 22:27:28 +0800

    fix: better prisma instanstation

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,17 @@
 import { PrismaClient } from '@/generated/prisma'
 
-const prisma = new PrismaClient()
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined
+}
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ['error', 'warn'],
+  })
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma
+}
+
 export default prisma
